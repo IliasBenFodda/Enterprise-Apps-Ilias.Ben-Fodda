bfddd674b147c6753f908237b526c137b92b67d1
Ben-Fodda <ilias.benfodda@student.odisee.be>
2026-06-06 12:31:59 +0200

    Test pagina + database configuratie

---
+++ src/main/java/be/ehb/enterpriseapplcations/enterpriseappsiliasbenfodda/controller/HomeController.java
@@ -0,0 +1,14 @@
+package be.ehb.enterpriseapplcations.enterpriseappsiliasbenfodda.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
